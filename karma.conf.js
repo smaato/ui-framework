bb50a5bef23b1aa0ae06f5f45bf9641ed3488e8e
CJ Cenizal <cj@cenizal.com>
2015-08-31 16:44:23 -0700

    Add Gulp build process. Create src/framework and src/examples directories. Update README.

---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var istanbul = require('browserify-istanbul');
+
+module.exports = function(config) {
+  config.set({
+    frameworks: [
+      'phantomjs-shim',
+      'browserify',
+      'jasmine'
+    ],
+    basePath: '',
+    // Specify the JS to compile to create a functioning test environment.
+    files: [
+      'node_modules/react/dist/react-with-addons.js',
+      'node_modules/babel-core/browser-polyfill.js',
+      'src/framework/**/*.jsx',
+      'src/framework/**/*.js'
+    ],
+    exclude: [
+    ],
+    browsers: ['PhantomJS'],
+    // The reporters with which to surface test results.
+    reporters: [
+      'dots',
+      'coverage'
+    ],
+    // Specify preprocessors through which files should be run, excluding test
+    // files.
+    preprocessors: {
+      'src/framework/**/*.js': ['browserify'],
+      'src/framework/**/*.jsx': ['browserify']
+    },
+    // Configuration for the above `browserify` preprocessor.
+    browserify: {
+      debug: true,
+      transform: ['babelify', 'browserify-istanbul']
+    }
+  });
+};
