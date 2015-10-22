96638c8f12b3ba92a167dfa1ee0e22fa83280579
Dmitry Antonov <antonov.dmitriy@gmail.com>
2015-10-22 16:52:56 +0200

    [BT-83] Core for icons

--- src/framework/framework.js
+++ src/framework/framework.js
@@ -2,6 +2,9 @@
 // Import components.
 import CheckBox from './checkBox/CheckBox.jsx';
 import Grid from './grid/Grid.jsx';
+import Icon from './icon/Icon.jsx';
+import IconCog from './iconCog/IconCog.jsx';
+import IconEllipsis from './iconEllipsis/IconEllipsis.jsx';
 import Spinner from './spinner/Spinner.jsx';
 import TitleBar from './titleBar/TitleBar.jsx';
 import TitleBarButton from './titleBar/TitleBarButton.jsx';
@@ -10,6 +13,9 @@ import TitleBarButton from './titleBar/TitleBarButton.jsx';
 export default {
   CheckBox: CheckBox,
   Grid: Grid,
+  Icon: Icon,
+  IconCog: IconCog,
+  IconEllipsis: IconEllipsis,
   Spinner: Spinner,
   TitleBar: TitleBar,
   TitleBarButton: TitleBarButton,
