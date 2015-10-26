6cdecefae7430f524d50d18df74808e1375d6c35
Dmitry Antonov <antonov.dmitriy@gmail.com>
2015-10-26 17:13:19 +0100

    [BT-107] Renamed folders and files in examples folder to camelCase

--- src/examples/_partials/navigation.js
+++ src/examples/_partials/navigation.js
@@ -13,18 +13,28 @@ $(() => {
     'titleBar',
   ];
 
+  // Borrowed from here http://stackoverflow.com/a/2970667
+  function toCamelCase(str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index) => {
+      if (+match === 0) return '';
+      return index === 0 ? match.toLowerCase() : match.toUpperCase();
+    });
+  }
+
   function addNavItems(source, rootDir) {
-    let value;
+    let pageName;
+    let pageNameCamelCase;
     let markup;
     let button;
     for (let i = 0; i < source.length; i++) {
-      value = source[i];
+      pageName = source[i];
+      pageNameCamelCase = toCamelCase(pageName);
       markup =
         `<a
           class="examplesNavButton"
-          href="/${rootDir}/${value}/${value}.html"
+          href="/${rootDir}/${pageNameCamelCase}/${pageNameCamelCase}.html"
         >
-          ${value}
+          ${pageName}
         </a>`;
       button = $(markup);
       menu.append(button);
--- src/examples/index.js
+++ src/examples/index.js
@@ -1,12 +1,12 @@
 
 import './_partials/navigation.js';
 
-import checkBox from './components/CheckBox/checkBox.js';
-import grid from './components/Grid/grid.js';
-import spinner from './components/Spinner/spinner.js';
+import checkBox from './components/checkBox/checkBox.js';
+import grid from './components/grid/grid.js';
+import spinner from './components/spinner/spinner.js';
 import titleBar from './components/titleBar/titleBar.js';
 
-// This holds scripts for each example page
+// Holds scripts for each example page
 const _pages = {
   checkBox,
   grid,
@@ -14,10 +14,11 @@ const _pages = {
   titleBar,
 };
 
-// This is just a wrapper to hold all global stuff, to pollute less
+// A wrapper to hold all global stuff
 const app = {
   // This executes scripts for a specific page
   render: (key) => _pages[key](),
 };
 
+// The only global variable we export to window
 window.app = app;
