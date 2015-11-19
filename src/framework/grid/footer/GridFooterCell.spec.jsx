c59ff782aaa11271734fcf431ba19785c580a6f1
Jens Lonkowski <jens.lonkowski@smaato.com>
2015-11-19 12:02:44 +0100

    [BT-101]: Improved GridFooterCell unit tests, GridHeaderCell unit tests

--- src/framework/grid/footer/GridFooterCell.spec.jsx
+++ src/framework/grid/footer/GridFooterCell.spec.jsx
@@ -4,18 +4,6 @@ import { TestCaseFactory } from 'react-test-kit';
 import GridFooterCell from './GridFooterCell.jsx';
 
 describe('GridFooterCell', () => {
-  describe('DOM structure', () => {
-    it('consists of two div elements with classes', () => {
-      const testCase = TestCaseFactory.createFromElement(<GridFooterCell />);
-      expect(testCase.dom.tagName).toBe('DIV');
-      expect(testCase.dom.getAttribute('class')).toBe('grid__footer__cell');
-      expect(testCase.dom.textContent).toBe('');
-
-      const innerDiv = testCase.first('div');
-      expect(innerDiv.tagName).toBe('DIV');
-      expect(innerDiv.getAttribute('class')).toBe('grid__footer__cellLiner');
-    });
-  });
   describe('Props', () => {
     describe('classFooterCell', () => {
       it('adds a class when set', () => {
@@ -23,12 +11,13 @@ describe('GridFooterCell', () => {
           classFooterCell: 'test',
         };
         const testCase = TestCaseFactory.createFromElement(<GridFooterCell {...props} />);
-        expect(testCase.dom.getAttribute('class')).toContain('test');
+
+        expect(testCase.dom.getAttribute('class')).toBe('grid__footer__cell test');
       });
     });
 
     describe('content', () => {
-      it('is rendered when set to string', () => {
+      it('is rendered as textContent when it\'s a string', () => {
         const props = {
           content: 'Test',
         };
@@ -37,7 +26,7 @@ describe('GridFooterCell', () => {
         expect(testCase.dom.textContent).toBe('Test');
       });
 
-      it('is rendered when set to number', () => {
+      it('is rendered textContent when it\'s a number', () => {
         const props = {
           content: 1,
         };
@@ -46,7 +35,7 @@ describe('GridFooterCell', () => {
         expect(testCase.dom.textContent).toBe('1');
       });
 
-      it('is rendered when set to element', () => {
+      it('is rendered as a child when it\'s an element', () => {
         const props = {
           content: <span />,
         };
