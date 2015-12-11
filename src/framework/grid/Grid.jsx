smaato/ui-framework

commit 325eb9517ee8b1314cd8b0acbaadfb6bde6fdfde

[BT-103]: Remove unused props from Grid Component. Add tests.

src/framework/grid/Grid.jsx
@@ -109,7 +109,9 @@ export default class Grid extends Component {
 
     // Update state of recycled rows when the user scrolls the table.
     const rowRecycler = this.props.rowRecycler;
-    this.setState(rowRecycler.getFirstRecycledRowState(this.state, scrollPosition));
+    if (rowRecycler) {
+      this.setState(rowRecycler.getFirstRecycledRowState(this.state, scrollPosition));
+    }
 
     // Lazily load rows as the user scrolls.
     if (this.props.lazyLoadRows) {
@@ -127,17 +129,29 @@ export default class Grid extends Component {
       firstRecycledRowOffset,
     } = this.state;
 
-    const {
-      lastRecycledRowIndex,
-      lastRecycledRowOffset,
-    } = this.props.rowRecycler.getLastRecycledRowState(this.state);
+    const rowsCount = this.props.rows.length;
+    // Set this to the rowsCount - 1 so that we can populate the table when
+    // there is no row recycler.
+    let lastRecycledRowIndex = rowsCount - 1;
+    let lastRecycledRowOffset = 0;
+
+    if (this.props.rowRecycler) {
+      const lastRecycledRowState =
+        this.props.rowRecycler.getLastRecycledRowState(this.state);
+      lastRecycledRowIndex = lastRecycledRowState.lastRecycledRowIndex;
+      lastRecycledRowOffset = lastRecycledRowState.lastRecycledRowOffset;
+    }
 
     // Create recycled rows.
-    const rowsCount = this.props.rows.length;
     const rows = [];
-    for (let index = firstRecycledRowIndex; index <= lastRecycledRowIndex && index < rowsCount; index++) {
-      const row = this.props.rows[index];
-      rows.push(row);
+    if (typeof this.props.rows === 'object') {
+      // We have one row, for some reason.
+      rows.push(this.props.rows);
+    } else {
+      for (let index = firstRecycledRowIndex; index <= lastRecycledRowIndex && index < rowsCount; index++) {
+        const row = this.props.rows[index];
+        rows.push(row);
+      }
     }
 
     // Style classes
@@ -154,17 +168,14 @@ export default class Grid extends Component {
             columnsCount={this.props.columnsCount}
             firstRecycledRowOffset={firstRecycledRowOffset}
             lastRecycledRowOffset={lastRecycledRowOffset}
-
             // Initial loading state
             initialLoadingRow={this.props.initialLoadingRow}
             // Loading state
             loadingRow={this.props.loadingRow}
             // Empty state
             emptyRow={this.props.emptyRow}
-
+            // Classes
             classBody={this.props.classBody}
-            classBodyRow={this.props.classBodyRow}
-            classBodyCell={this.props.classBodyCell}
           >
             {rows}
           </GridBody>
@@ -179,7 +190,6 @@ export default class Grid extends Component {
 }
 
 Grid.propTypes = {
-  id: PropTypes.string,
   columnsCount: GridBody.propTypes.columnsCount,
   header: PropTypes.element,
   footer: PropTypes.element,
@@ -191,15 +201,13 @@ Grid.propTypes = {
   emptyRow: PropTypes.element,
   // Loading state
   loadingRow: GridBody.propTypes.loadingRow,
-  onClickRow: PropTypes.func,
   rowRecycler: PropTypes.instanceOf(GridRowRecycler),
   // Classes
   classContainer: PropTypes.string,
   classTable: PropTypes.string,
   classBody: PropTypes.string,
-  classBodyRow: PropTypes.string,
-  classBodyCell: PropTypes.string,
-  classFooter: PropTypes.string,
-  classFooterRow: PropTypes.string,
-  classFooterCell: PropTypes.string,
+};
+
+Grid.defaultProps = {
+  rows: [],
 };

src/framework/grid/Grid.spec.js
@@ -1,0 +1,119 @@
+
+import React from 'react';
+import { TestCaseFactory } from 'react-test-kit';
+import Grid from './Grid.jsx';
+
+describe('Grid', () => {
+  describe('Props', () => {
+    describe('header', () => {
+      it('is rendered inside the table element', () => {
+        const props = {
+          columnsCount: 1,
+          header: <thead id="header"></thead>,
+        };
+
+        const testCase = TestCaseFactory.createFromElement(<Grid {...props} />);
+        expect(testCase.find('#header').length).toBe(1);
+      });
+    });
+
+    describe('footer', () => {
+      it('is rendered inside the table element', () => {
+        const props = {
+          columnsCount: 1,
+          footer: <tfoot id="header"></tfoot>,
+        };
+
+        const testCase = TestCaseFactory.createFromElement(<Grid {...props} />);
+        expect(testCase.find('#header').length).toBe(1);
+      });
+    });
+
+    describe('rows', () => {
+      it('is rendered inside the table element', () => {
+        const props = {
+          columnsCount: 1,
+          rows: <tr id="row" key="0"><td></td></tr>,
+        };
+
+        const testCase = TestCaseFactory.createFromElement(<Grid {...props} />);
+        expect(testCase.find('#row').length).toBe(1);
+      });
+    });
+
+    describe('initialLoadingRow', () => {
+      it('is rendered inside the table element', () => {
+        const props = {
+          columnsCount: 1,
+          initialLoadingRow: <tr id="initialLoadingRow"><td></td></tr>,
+        };
+
+        const testCase = TestCaseFactory.createFromElement(<Grid {...props} />);
+        expect(testCase.find('#initialLoadingRow').length).toBe(1);
+      });
+    });
+
+    describe('emptyRow', () => {
+      it('is rendered inside the table element', () => {
+        const props = {
+          columnsCount: 1,
+          emptyRow: <tr id="emptyRow"><td></td></tr>,
+        };
+
+        const testCase = TestCaseFactory.createFromElement(<Grid {...props} />);
+        expect(testCase.find('#emptyRow').length).toBe(1);
+      });
+    });
+
+    describe('loadingRow', () => {
+      it('is rendered inside the table element', () => {
+        const props = {
+          columnsCount: 1,
+          loadingRow: <tr id="loadingRow"><td></td></tr>,
+        };
+
+        const testCase = TestCaseFactory.createFromElement(<Grid {...props} />);
+        expect(testCase.find('#loadingRow').length).toBe(1);
+      });
+    });
+
+    describe('classContainer', () => {
+      it('is applied as a class of the root element', () => {
+        const props = {
+          columnsCount: 1,
+          classContainer: 'test',
+        };
+
+        const testCase = TestCaseFactory.createFromElement(<Grid {...props} />);
+        expect(testCase.dom.getAttribute('class')
+          .indexOf(props.classContainer) !== -1).toBe(true);
+      });
+    });
+
+    describe('classTable', () => {
+      it('is applied as a class of the table element', () => {
+        const props = {
+          columnsCount: 1,
+          classTable: 'test',
+        };
+
+        const testCase = TestCaseFactory.createFromElement(<Grid {...props} />);
+        expect(testCase.first('table').getAttribute('class')
+          .indexOf(props.classTable) !== -1).toBe(true);
+      });
+    });
+
+    describe('classBody', () => {
+      it('is applied as a class of the tbody element', () => {
+        const props = {
+          columnsCount: 1,
+          classBody: 'test',
+        };
+
+        const testCase = TestCaseFactory.createFromElement(<Grid {...props} />);
+        expect(testCase.first('tbody').getAttribute('class')
+          .indexOf(props.classBody) !== -1).toBe(true);
+      });
+    });
+  });
+});

src/framework/grid/body/GridBody.spec.jsx
@@ -13,7 +13,7 @@ describe('GridBody', () => {
     );
   }
 
-  fdescribe('Props', () => {
+  describe('Props', () => {
     describe('columnsCount', () => {
       it('is applied to colspan of first fake row', () => {
         const props = {

commit 482b07e124dffc15ee00777c67acad9320cb1eda

[BT-103]: Fix regression bug introduced in 325eb95, which incorrectly interpeted a rows array as a single row object.

src/framework/grid/Grid.jsx
@@ -144,14 +144,14 @@ export default class Grid extends Component {
 
     // Create recycled rows.
     const rows = [];
-    if (typeof this.props.rows === 'object') {
-      // We have one row, for some reason.
-      rows.push(this.props.rows);
-    } else {
+    if (Array.isArray(this.props.rows)) {
       for (let index = firstRecycledRowIndex; index <= lastRecycledRowIndex && index < rowsCount; index++) {
         const row = this.props.rows[index];
         rows.push(row);
       }
+    } else {
+      // We have one row, for some reason.
+      rows.push(this.props.rows);
     }
 
     // Style classes