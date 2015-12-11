770c3dfa492f3e27b23cba3fa568041a22ff389c
Jens Lonkowski <jens.lonkowski@smaato.com>
2015-12-11 16:21:08 +0100

    [BT-164]: Updated gulp-tasks to v0.3.1, updated task definitions, removed separate files and fixed scss warnings

---
+++ gulpfile.js
@@ -0,0 +1,188 @@
+
+/**
+ * @description Constants
+ *******************************************************************************
+ */
+
+const del = require('del');
+const gulp = require('gulp');
+const gulpReplace = require('gulp-replace');
+const gulpTasks = require('gulp-tasks');
+const runSequence = require('run-sequence');
+
+const DISTRIBUTION_DIR = './dist';
+const CSS_DST = `${DISTRIBUTION_DIR}/css`;
+const JS_DST = `${DISTRIBUTION_DIR}/js`;
+
+const SOURCE_DIR = './src';
+const ASSETS_SRC = `${SOURCE_DIR}/assets/**/*.*`;
+const JS_SRC = `${SOURCE_DIR}/guide/index.js`;
+const FRAMEWORK_SCSS_SRC = `${SOURCE_DIR}/framework/**/*.scss`;
+const GUIDE_SCSS_SRC = `${SOURCE_DIR}/guide/**/*.scss`;
+const PROTOTYPE_SCSS_SRC = `${SOURCE_DIR}/prototype/**/*.scss`;
+const TEMPLATES_SRC = `${SOURCE_DIR}/**/*.jade`;
+
+/**
+ * @description Main tasks
+ *******************************************************************************
+ */
+
+gulp.task('assets', gulpTasks.copy({
+  dst: `${DISTRIBUTION_DIR}/assets`,
+  src: ASSETS_SRC,
+}).task);
+
+gulp.task('deploy', gulpTasks.deploy({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  bucketName: process.env.AWS_BUCKET_UI_FRAMEWORK,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  src: `${DISTRIBUTION_DIR}/**/*.*`,
+}).task);
+
+gulp.task('scripts', gulpTasks.compileJs({
+  dst: JS_DST,
+  src: JS_SRC,
+}).task);
+
+gulp.task('styles', gulpTasks.compileCss({
+  compassSassDir: `${SOURCE_DIR}/guide`,
+  dst: CSS_DST,
+  src: GUIDE_SCSS_SRC,
+  subTaskPrefix: 'styles',
+}).task);
+
+gulp.task('styles:prototype', gulpTasks.compileCss({
+  compassSassDir: `${SOURCE_DIR}/prototype`,
+  dst: `${DISTRIBUTION_DIR}/prototype`,
+  src: PROTOTYPE_SCSS_SRC,
+  subTaskPrefix: 'styles:prototype',
+}).task);
+
+gulp.task('templates', ['templates:index'], gulpTasks.compileHtml({
+  dst: DISTRIBUTION_DIR,
+  src: [
+    TEMPLATES_SRC,
+    `!${SOURCE_DIR}/guide/index.jade`,
+  ],
+}).task);
+
+gulp.task('templates:index', gulpTasks.compileHtml({
+  dst: DISTRIBUTION_DIR,
+  src: `${SOURCE_DIR}/guide/index.jade`,
+}).task);
+
+/**
+ * @description Development tasks
+ *******************************************************************************
+ */
+
+gulp.task('scriptsThenWatch', gulpTasks.compileJs({
+  dst: JS_DST,
+  src: JS_SRC,
+  watch: true,
+}).task);
+
+gulp.task('serveLocally', gulpTasks.serve({
+  fallback: `${DISTRIBUTION_DIR}/index.html`,
+  livereload: true,
+  port: 8001,
+  root: DISTRIBUTION_DIR,
+}).task);
+
+gulp.task('watch', [
+  'templates',
+  'assets',
+  'styles',
+  'styles:prototype',
+  'scriptsThenWatch',
+  'serveLocally',
+], () => {
+  gulp.watch([TEMPLATES_SRC], ['templates']);
+  gulp.watch([ASSETS_SRC], ['assets']);
+  gulp.watch([
+    FRAMEWORK_SCSS_SRC,
+    GUIDE_SCSS_SRC,
+  ], ['styles']);
+  gulp.watch([PROTOTYPE_SCSS_SRC], ['styles:prototype']);
+});
+
+gulp.task('default', () => {
+  process.env.NODE_ENV = 'developmentWithHmr';
+
+  gulp.start('watch');
+});
+
+/**
+ * @description Production tasks
+ *******************************************************************************
+ */
+
+gulp.task('minifyCss', gulpTasks.minifyCss({
+  src: CSS_DST,
+}).task);
+
+gulp.task('minifyJs', gulpTasks.minifyJs({
+  src: JS_DST,
+}).task);
+
+gulp.task('replace', () => {
+  return gulp.src([`${DISTRIBUTION_DIR}/index.html`])
+    .pipe(gulpReplace(/\.css/, '.min.css'))
+    .pipe(gulpReplace(/\.js/, '.min.js'))
+    .pipe(gulp.dest(DISTRIBUTION_DIR));
+});
+
+gulp.task('clean', (callback) => {
+  return del([
+    `${CSS_DST}/dist.css`,
+    `${JS_DST}/dist.js`,
+  ], callback);
+});
+
+gulp.task('production', (callback) => {
+  process.env.NODE_ENV = 'production';
+
+  runSequence(
+    'templates',
+    'assets',
+    'styles',
+    'styles:prototype',
+    'scripts',
+    ['minifyCss', 'minifyJs'],
+    'replace',
+    'clean',
+    callback
+  );
+});
+
+/**
+ * @description Test tasks
+ *******************************************************************************
+ */
+
+gulp.task('lintJs', gulpTasks.lintJs({
+  src: [
+    `${SOURCE_DIR}/framework/**/*.jsx`,
+    `${SOURCE_DIR}/framework/**/*.js`,
+    `${SOURCE_DIR}/guide/**/*.jsx`,
+    `${SOURCE_DIR}/guide/**/*.js`,
+  ],
+}).task);
+
+gulp.task('lintScss', gulpTasks.lintScss({
+  file: './scss-config.yml',
+  src: [
+    FRAMEWORK_SCSS_SRC,
+    GUIDE_SCSS_SRC,
+  ],
+}).task);
+
+gulp.task('testUnit', gulpTasks.testUnit({
+  configFile: `${__dirname}/karma.conf.js`,
+}).task);
+
+gulp.task('test', [
+  'lintJs',
+  'lintScss',
+  'testUnit',
+]);
