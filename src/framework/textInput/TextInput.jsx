9b4a27d52d3e0bd08a8366c6deb9722d5bef70b9
CJ Cenizal <cj@cenizal.com>
2015-12-11 08:11:04 -0800

    [BT-169]: Rename TextInput prop classes to className.

--- src/framework/textInput/TextInput.jsx
+++ src/framework/textInput/TextInput.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import classNames from 'classnames';
 
 const TextInput = props => {
-  const classes = classNames('textInput', props.classes);
+  const classes = classNames('textInput', props.className);
 
   const extendedProps = Object.assign({}, props, {
     className: classes,
