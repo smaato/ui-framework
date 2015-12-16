00a280c0c15313d0ada986c872def657489331c5
Dmitry Antonov <antonov.dmitriy@gmail.com>
2015-12-16 15:42:33 +0100

    [BT-167] Removed modalBody styles. Allow scrolling modal that is smaller than window

--- src/framework/modal/ModalOverlay.jsx
+++ src/framework/modal/ModalOverlay.jsx
@@ -1,25 +1,65 @@
 
 import React, {
+  Component,
   PropTypes,
 } from 'react';
 
 import classNames from 'classnames';
 
-const ModalOverlay = props => {
-  const overlayClasses = classNames('modalOverlay', {
-    'is-modal-overlay-open': props.isOpen,
-  });
+export default class ModalOverlay extends Component {
 
-  return (
-    <div className={overlayClasses}>
-      {props.children}
-    </div>
-  );
-};
+  constructor(props) {
+    super(props);
+    this.resizeListenerBound = this.resizeListener.bind(this);
+    this.state = {
+      isWindowSmallerThanModal: false,
+    };
+  }
+
+  componentDidMount() {
+    this.resizeListenerBound();
+    window.addEventListener('resize', this.resizeListenerBound);
+  }
+
+  componentDidUpdate() {
+    this.resizeListenerBound();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resizeListenerBound);
+  }
+
+  isWindowSmallerThanModal() {
+    const windowHeight = window.innerHeight;
+    const modalEl = this.refs.overlayEl.children[0];
+    const modalHeight = modalEl.offsetHeight;
+    return windowHeight <= modalHeight;
+  }
+
+  resizeListener() {
+    const isWindowSmaller = this.isWindowSmallerThanModal();
+    if (this.state.isWindowSmallerThanModal !== isWindowSmaller) {
+      this.setState({
+        isWindowSmallerThanModal: isWindowSmaller,
+      });
+    }
+  }
+
+  render() {
+    const overlayClasses = classNames('modalOverlay', {
+      'is-modal-overlay-open': this.props.isOpen,
+      'is-window-smaller-than-modal': this.state.isWindowSmallerThanModal,
+    });
+
+    return (
+      <div className={overlayClasses} ref="overlayEl">
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 ModalOverlay.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   children: PropTypes.element.isRequired,
 };
-
-export default ModalOverlay;
