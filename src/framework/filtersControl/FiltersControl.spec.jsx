002748431a204c4a5bf24722085f3a5d855d4236
Jens Lonkowski <jens.lonkowski@smaato.com>
2016-01-28 14:14:02 +0100

    [BT-268]: Added two tests for FiltersControl

---
+++ src/framework/filtersControl/FiltersControl.spec.jsx
@@ -0,0 +1,56 @@
+
+import { TestCaseFactory } from 'react-test-kit';
+import FiltersControl from './FiltersControl.jsx';
+import {
+  ConditionChecker,
+} from '../services';
+
+describe('FiltersControl', () => {
+  describe('Props', () => {
+    describe('conditionCheckers', () => {
+      it('are passed to ConditionCheckerList and iterated over', () => {
+        const props = {
+          conditionCheckers: [
+            new ConditionChecker({}),
+          ],
+          filterOptions: [],
+          onAddConditionChecker: () => {},
+          onRemoveConditionChecker: () => {},
+        };
+
+        const iterationSpy = spyOn(props.conditionCheckers, 'map');
+
+        expect(iterationSpy).not.toHaveBeenCalled();
+        TestCaseFactory.createFromClass(FiltersControl, props);
+        expect(iterationSpy).toHaveBeenCalled();
+      });
+    });
+
+    describe('onRemoveConditionChecker', () => {
+      it('is passed to ConditionCheckerList and called with a conditionChecker when a remove button is clicked', () => {
+        const props = {
+          conditionCheckers: [
+            new ConditionChecker({}),
+          ],
+          filterOptions: [],
+          onAddConditionChecker: () => {},
+          onRemoveConditionChecker: jasmine.createSpy('onRemoveConditionChecker'),
+        };
+
+        const testCase =
+          TestCaseFactory.createFromClass(FiltersControl, props);
+
+        const removeButton =
+          testCase.first('.conditionCheckerListItem__removeButton');
+
+        expect(props.onRemoveConditionChecker).not.toHaveBeenCalled();
+        testCase.trigger('click', removeButton);
+        expect(props.onRemoveConditionChecker).toHaveBeenCalledWith(
+          props.conditionCheckers[0],
+          jasmine.any(Object), // SyntheticEvent
+          jasmine.any(String) // React ID
+        );
+      });
+    });
+  });
+});
