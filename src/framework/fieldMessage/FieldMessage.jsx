86ceaa78816c1b1ca66d875838f4206d80482b44
Dmitry Antonov <antonov.dmitriy@gmail.com>
2016-02-16 14:57:38 +0100

    [BT-361] Converted to a wrapper component. Taking width from child. isDisplayed prop

--- src/framework/fieldMessage/FieldMessage.jsx
+++ src/framework/fieldMessage/FieldMessage.jsx
@@ -5,21 +5,42 @@ import React, {
 import classNames from 'classnames';
 
 const FieldMessage = props => {
-  const classes = classNames('fieldMessage', props.className);
+  const classes = classNames('fieldMessage', props.className, {
+    'does-field-message-have-childs-width': props.hasChildsWidth,
+  });
 
   const extendedProps = Object.assign({}, props, {
     className: classes,
   });
 
+  let balloon;
+
+  if (props.isDisplayed) {
+    balloon = (
+      <div className="fieldMessageBalloon">
+        {props.message}
+      </div>
+    );
+  }
+
   return (
     <div {...extendedProps}>
       {props.children}
+      {balloon}
     </div>
   );
 };
 
 FieldMessage.propTypes = {
-  children: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+  message: PropTypes.string.isRequired,
+  hasChildsWidth: PropTypes.bool,
+  isDisplayed: PropTypes.bool,
+};
+
+FieldMessage.defaultProps = {
+  hasChildsWidth: false,
+  isDisplayed: false,
 };
 
 export default FieldMessage;
--- src/guide/views/fieldMessage/FieldMessageExample.jsx
+++ src/guide/views/fieldMessage/FieldMessageExample.jsx
@@ -8,6 +8,8 @@ import Page, {
 } from '../../components/page/Page.jsx';
 
 import {
+  AddOnControl,
+  CallOutButton,
   FieldMessage,
   LabeledControl,
   TextInput,
@@ -17,25 +19,71 @@ export default class FieldMessageExample extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      isErrorDisplayed: true,
+    };
+  }
+
+  onButtonClick() {
+    this.setState({
+      isErrorDisplayed: !this.state.isErrorDisplayed,
+    });
   }
 
   render() {
     return (
       <Page title={this.props.route.name}>
 
-        <Example>
+        <Example title="TextInput">
           <LabeledControl
             label="Name"
-            layout={LabeledControl.LAYOUT.TWO_FIFTHS}
+            layout={LabeledControl.LAYOUT.ONE_SIXTH}
           >
-            <TextInput
-              isError
-              isFullWidth
-            />
-            <FieldMessage>
-              Please enter a name for this endpoint.
+            <FieldMessage
+              message="Please enter a name for this endpoint."
+              isDisplayed={this.state.isErrorDisplayed}
+            >
+              <TextInput
+                isError={this.state.isErrorDisplayed}
+                isFullWidth
+              />
             </FieldMessage>
           </LabeledControl>
+
+          <CallOutButton
+            label={
+              `${this.state.isErrorDisplayed ? 'Hide' : 'Show'} field message.`
+            }
+            onClick={this.onButtonClick.bind(this)}
+          />
+        </Example>
+
+        <Example title="AddOnControl">
+          <LabeledControl
+            label="QPS Limit"
+            layout={LabeledControl.LAYOUT.ONE_SIXTH}
+          >
+            <FieldMessage
+              message="Please enter a valid number."
+              isDisplayed={this.state.isErrorDisplayed}
+              hasChildsWidth
+            >
+              <AddOnControl
+                right="queries per second"
+              >
+                <TextInput
+                  isError={this.state.isErrorDisplayed}
+                />
+              </AddOnControl>
+            </FieldMessage>
+          </LabeledControl>
+
+          <CallOutButton
+            label={
+              `${this.state.isErrorDisplayed ? 'Hide' : 'Show'} field message.`
+            }
+            onClick={this.onButtonClick.bind(this)}
+          />
         </Example>
 
       </Page>
