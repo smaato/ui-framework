0df3d85632cf14011a76f473900e471b2c6e4bc2
Alan Hollesen <alan@smaato.com>
2016-03-01 11:24:16 +0100

    [BT-419] Three column layout component - implemented flat column layout - no tests added, as might not be used

--- src/framework/columnLayout/ColumnLayout.jsx
+++ src/framework/columnLayout/ColumnLayout.jsx
@@ -1,15 +1,55 @@
 
-import React from 'react';
+import React, {
+  PropTypes,
+} from 'react';
 import classNames from 'classnames';
 
 const ColumnLayout = props => {
+  const layoutClassMap = {
+    [ColumnLayout.LAYOUT.AUTO]: 'columnLayout--auto',
+    [ColumnLayout.LAYOUT.HALF]: 'columnLayout--half',
+    [ColumnLayout.LAYOUT.ONE_THIRD]: 'columnLayout--oneThird',
+    [ColumnLayout.LAYOUT.ONE_FOURTH]: 'columnLayout--oneFourth',
+    [ColumnLayout.LAYOUT.ONE_FIFTH]: 'columnLayout--oneFifth',
+    [ColumnLayout.LAYOUT.TWO_FIFTHS]: 'columnLayout--twoFifths',
+    [ColumnLayout.LAYOUT.ONE_SIXTH]: 'columnLayout--oneSixth',
+  };
+
   const classes = classNames('columnLayout');
+
+  function wrapChildren() {
+    return props.children.map((entry, index) => (
+      <div
+        key={'column_' + `${index}`}
+        className={layoutClassMap[props.layout[index]] ||
+          'columnLayout--auto'}
+      >
+        {entry}
+      </div>
+    ));
+  }
+
+  const wrappedChildren = wrapChildren();
 
   return (
     <div className={classes}>
-        {props.children}
+      {wrappedChildren}
     </div>
   );
+};
+
+ColumnLayout.LAYOUT = {
+  AUTO: 'AUTO',
+  HALF: 'HALF',
+  ONE_THIRD: 'ONE_THIRD',
+  ONE_FOURTH: 'ONE_FOURTH',
+  ONE_FIFTH: 'ONE_FIFTH',
+  TWO_FIFTHS: 'TWO_FIFTHS',
+  ONE_SIXTH: 'ONE_SIXTH',
+};
+
+ColumnLayout.propTypes = {
+  layout: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default ColumnLayout;
--- src/guide/views/columnLayout/ColumnLayoutExample.jsx
+++ src/guide/views/columnLayout/ColumnLayoutExample.jsx
@@ -18,11 +18,67 @@ export default class ColumnLayoutExample extends Component {
   }
 
   render() {
+    const backgroundColor = {
+      backgroundColor: '#66b3ff',
+    };
+
     return (
       <Page title={this.props.route.name}>
-        <Example>
-          <ColumnLayout />
+
+        <Example title="1/3 - auto">
+          <ColumnLayout
+            layout={[
+              ColumnLayout.LAYOUT.ONE_THIRD,
+            ]}
+          >
+            <div style={backgroundColor}>one-third</div>
+            <div style={backgroundColor}>auto</div>
+          </ColumnLayout>
         </Example>
+
+        <Example title="1/6 - 1/2 - auto">
+          <ColumnLayout
+            layout={[
+              ColumnLayout.LAYOUT.ONE_SIXTH,
+              ColumnLayout.LAYOUT.HALF,
+            ]}
+          >
+            <div style={backgroundColor}>one-sixth</div>
+            <div style={backgroundColor}>half</div>
+            <div style={backgroundColor}>auto</div>
+          </ColumnLayout>
+        </Example>
+
+        <Example title="1/3 - auto - 1/4">
+          <ColumnLayout
+            layout={[
+              ColumnLayout.LAYOUT.ONE_THIRD,
+              ColumnLayout.LAYOUT.AUTO,
+              ColumnLayout.LAYOUT.ONE_FOURTH,
+            ]}
+          >
+            <div style={backgroundColor}>one-third</div>
+            <div style={backgroundColor}>auto</div>
+            <div style={backgroundColor}>one-fourth</div>
+          </ColumnLayout>
+        </Example>
+
+        <Example title="1/5 - 1/4 - auto - 2/5">
+          <ColumnLayout
+            layout={[
+              ColumnLayout.LAYOUT.ONE_FIFTH,
+              ColumnLayout.LAYOUT.ONE_FOURTH,
+              ColumnLayout.LAYOUT.AUTO,
+              ColumnLayout.LAYOUT.TWO_FIFTHS,
+            ]}
+          >
+            <div style={backgroundColor}>one-fifth</div>
+            <div style={backgroundColor}>one-fourth</div>
+            <div style={backgroundColor}>auto</div>
+            <div style={backgroundColor}>two-fifths</div>
+          </ColumnLayout>
+        </Example>
+
       </Page>
     );
   }
