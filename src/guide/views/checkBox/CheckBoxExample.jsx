d08fd1f51521ca6e618157384da14069e620e338
CJ Cenizal <cj@cenizal.com>
2016-03-14 11:48:05 -0700

    [BT-458]: Reimplement CheckBox component. - Pass checked state, id, and data prop to onClick handler. - Stop click events from bubbling up, fixing interference with GridRow's onClick handler.

--- src/guide/views/checkBox/CheckBoxExample.jsx
+++ src/guide/views/checkBox/CheckBoxExample.jsx
@@ -14,13 +14,31 @@ export default class CheckBoxExample extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      isChecked: true,
+    };
+
+    this.onClickCheckBox = this.onClickCheckBox.bind(this);
+  }
+
+  onClickCheckBox(isChecked, id, data) {
+    window.alert(`${id} ${data}`); // eslint-disable-line no-alert
+    this.setState({
+      isChecked,
+    });
   }
 
   render() {
     return (
       <Page title={this.props.route.name}>
         <Example>
-          <CheckBox id="checkboxExample" />
+          <CheckBox
+            id="checkboxId"
+            onClick={this.onClickCheckBox}
+            checked={this.state.isChecked}
+            data="Checkbox Data"
+          />
         </Example>
       </Page>
     );
