d18f6566c06b788dae74e671ec6bafbe6256311b
Alan Hollesen <alan@smaato.com>
2016-11-28 11:49:13 +0100

    [SDX-1026] Implement inventory discovery view logic - adjusted formatBigNumber to handle negative numbers as well + tests

--- src/framework/services/number/Number.js
+++ src/framework/services/number/Number.js
@@ -6,8 +6,9 @@ const POWER_TO_LABEL_MAP = {
 };
 
 function formatBigNumber(number) {
+  const absolute = Math.abs(number);
   // Floor of log (base 1000) of number, e.g. 1,234,567 -> 2
-  const power = Math.floor(Math.log(number) / Math.log(1000));
+  const power = Math.floor(Math.log(absolute) / Math.log(1000));
   const label = POWER_TO_LABEL_MAP[power];
 
   if (label) {
