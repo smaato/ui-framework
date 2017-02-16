317776b3befc2d1ba826d1a73df529a795b9307d
Olga Andreeva <olga.andreeva@smaato.com>
2017-02-16 16:25:29 +0100

    SDX-1165 Add bodyPanel--bottomFlush class

--- src/framework/body/BodyPanel.jsx
+++ src/framework/body/BodyPanel.jsx
@@ -9,6 +9,7 @@ import Box from '../box/Box.jsx';
 const BodyPanel = (props) => {
   const classes = classNames('bodyPanel', {
     'bodyPanel--topFlush': props.isTopFlush,
+    'bodyPanel--bottomFlush': props.isBottomFlush,
   });
 
   return (
@@ -24,6 +25,7 @@ const BodyPanel = (props) => {
 BodyPanel.propTypes = {
   children: Box.propTypes.children,
   isTopFlush: PropTypes.bool,
+  isBottomFlush: PropTypes.bool,
   roundedCorners: Box.propTypes.roundedCorners,
 };
 
