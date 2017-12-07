eff09b25f23785c13cb8dcac6fecd9834ec00f82
Maria Sancho <mariacasancho@gmail.com>
2017-12-07 13:29:48 +0100

    [SEM-26] Create list view for mapped external inventory: - alert button implemented.

---
+++ src/framework/button/AlertButton.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import classNames from 'classnames';
+
+import Button from './Button.jsx';
+
+const AlertButton = (props) => {
+  const classes = classNames('button--alert', props.classes);
+
+  const extendedProps = Object.assign({}, props, {
+    classes,
+  });
+
+  return (
+    <Button {...extendedProps} />
+  );
+};
+
+AlertButton.TYPE = Object.assign({}, Button.TYPE);
+
+AlertButton.propTypes = Object.assign({}, Button.propTypes);
+
+export default AlertButton;
--- src/guide/views/button/ButtonExample.jsx
+++ src/guide/views/button/ButtonExample.jsx
@@ -10,6 +10,7 @@ import Page, {
 } from '../../components/page/Page.jsx';
 
 import {
+  AlertButton,
   BasicButton,
   Button,
   CallOutButton,
@@ -77,6 +78,17 @@ export default class ButtonExample extends Component {
           <PrimaryButton>
             Save
           </PrimaryButton>
+        </Example>
+
+        <Example title="AlertButton">
+          <Text>
+            This button lets the user perform a special action, like
+            &quot;Cancel&quot;. It&apos;s higher in the UI hierarchy than the
+            BasicButton.
+          </Text>
+          <AlertButton>
+            Cancel
+          </AlertButton>
         </Example>
 
         <Example title="CallOutButton">
