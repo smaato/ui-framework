0edd1fa544488d879f6a66b7c231a7b3630fd39e
Arijit Dasgupta <arijit.dasgupta@smaato.com>
2018-04-24 15:01:01 +0200

    [SDX-1524] WIP Card component

--- src/framework/card/Card.jsx
+++ src/framework/card/Card.jsx
@@ -4,8 +4,44 @@ import React, {
   PropTypes,
 } from 'react';
 
+import {
+  Label,
+  SubLabel,
+  Tooltip
+} from '../framework';
+
 const Card = (props) => {
-  return <p>BOOM!</p>;
-}
+  const style = {
+    width: props.width || '212px;',
+    height: props.height || '247px;'
+  }
+
+  return <div className='card'>
+    <figure>
+      <img style={{width:`${props.width}`}} src={props.imageSrc} />
+    </figure>
+    <div className='card__info'>
+      <Label>{props.title}</Label>
+      <SubLabel>{props.subtitle}</SubLabel>
+    </div>
+    <div className='card__footer'>
+      <div>
+        <Tooltip message={props.tooltipText} width='200px;'>
+          <Label className='green'>{props.hightlightText}</Label>
+        </Tooltip>
+      </div>
+    </div>
+  </div>;
+};
+
+Card.propTypes = {
+  imageSrc: PropTypes.string,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  hightlightText: PropTypes.string,
+  tooltipText: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string
+};
 
 export default Card;
--- src/guide/views/card/CardExample.jsx
+++ src/guide/views/card/CardExample.jsx
@@ -17,7 +17,15 @@ export default class CardExample extends Component {
   render() {
     return <Page title={this.props.route.name}>
       <Example>
-        <Card />
+        <Card
+          imageSrc='https://www.monkeyworlds.com/wp-content/uploads/capuchin_species.jpg'
+          title='This is a title'
+          subtitle='This is a subtitle'
+          hightlightText='Highlight'
+          tooltipText='BOOM!'
+          width='200px'
+          height='300px'
+        />
       </Example>
     </Page>
   }
