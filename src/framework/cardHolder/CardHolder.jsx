088caee615fff4179c476215dd142030afd1e8fe
Arijit Dasgupta <arijit.dasgupta@smaato.com>
2018-04-30 12:25:01 +0200

    [SDX-1525] Card Holder component

--- src/framework/cardHolder/CardHolder.jsx
+++ src/framework/cardHolder/CardHolder.jsx
@@ -2,18 +2,28 @@ import React, {
   PropTypes,
 } from 'react';
 
-const CardHolder = props => <div className="card-holder">
-  {props.children.map((child, index) => {
-    return (
-      <div key={index} className="card-holder-card-wrapper">
-        {child}
-      </div>
-    );
-  })}
-</div>;
+const CardHolder = (props) => {
+  const childrenMinWidth = props.childrenMinWidth
+  ? props.childrenMinWidth : '240px';
+
+  const holderStyle = {
+    gridTemplateColumns: `repeat(auto-fit, minmax(${childrenMinWidth}, 1fr))`,
+  };
+
+  return (
+    <div className="card-holder" style={holderStyle}>
+      {props.children.map((child, index) => (
+        <div key={index} className="card-holder-card-wrapper">
+          {child}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 CardHolder.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element),
+  childrenMinWidth: PropTypes.string,
 };
 
 export default CardHolder;
---
+++ src/framework/cardHolder/CardHolder.spec.jsx
@@ -0,0 +1,8 @@
+import { TestCaseFactory } from 'react-test-kit';
+import CardHolder from './CardHolder.jsx';
+
+describe('CardHolder', () => {
+  describe('DOM structure', () => {
+    
+  });
+});
