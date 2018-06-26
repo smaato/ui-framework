95c380a8b3b3bf79860710649d81a144e2d5b8d5
Tomasz Janiczek <t.janiczek@clearcode.cc>
2018-06-26 15:45:51 +0200

    TF-392: Fix circular dependencies in Button and Kpi components

---
+++ src/framework/kpi/index.jsx
@@ -0,0 +1,11 @@
+export {
+    default as Kpi,
+} from './Kpi.jsx';
+
+export {
+  default as KpiPositive,
+} from './KpiPositive.jsx';
+
+export {
+  default as KpiNegative,
+} from './KpiNegative.jsx';
