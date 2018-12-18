9f20fdc4818a83aab9134275db93c423a37a9b86
Denis Yaremov <denis.yaremov@gmail.com>
2018-12-18 17:36:39 +0200

    Another refactoring as per Dario's request

--- src/framework/pagination/Pagination.spec.jsx
+++ src/framework/pagination/Pagination.spec.jsx
@@ -3,215 +3,245 @@ import { TestCaseFactory } from 'react-test-kit';
 import Pagination from './Pagination.jsx';
 
 describe('Pagination', () => {
-  function expectDomTextContentForProps(props) {
+  const render = (props, textContent = null) => {
     const testCase = TestCaseFactory.create(Pagination, props);
-    return expect(testCase.dom.textContent);
-  }
+    const element = textContent === null ? null : testCase.find('li').find(
+      e => e.textContent === textContent
+    );
+
+    return {
+      props,
+      element,
+      testCase,
+    };
+  };
 
   describe('Props', () => {
     describe('currentPage, totalPages and visiblePages', () => {
       it('are rendered accordingly', () => {
-        expectDomTextContentForProps({
+        const { testCase } = render({
           currentPage: 3,
           totalPages: 123,
           visiblePages: 3,
-        }).toBe('<Prev345Next>');
+        });
+
+        expect(testCase.dom.textContent).toBe('<Prev345Next>');
       });
     });
   });
 
   describe('First selected page', () => {
     it('is rendered properly', () => {
-      expectDomTextContentForProps({
+      const { testCase } = render({
         currentPage: 0,
         totalPages: 123,
         visiblePages: 3,
-      }).toBe('<Prev123Next>');
+      });
+
+      expect(testCase.dom.textContent).toBe('<Prev123Next>');
     });
   });
 
   describe('Last selected page', () => {
     it('is rendered properly', () => {
-      expectDomTextContentForProps({
+      const { testCase } = render({
         currentPage: 122,
         totalPages: 123,
         visiblePages: 3,
-      }).toBe('<Prev121122123Next>');
+      });
+
+      expect(testCase.dom.textContent).toBe('<Prev121122123Next>');
     });
   });
 
   describe('Small amount of pages', () => {
     it('should be rendered properly', () => {
-      expectDomTextContentForProps({
+      const { testCase } = render({
         currentPage: 2,
         totalPages: 3,
         visiblePages: 5,
-      }).toBe('<Prev123Next>');
+      });
+
+      expect(testCase.dom.textContent).toBe('<Prev123Next>');
     });
   });
 
-  function elementWithTextContentForProps(props, selector, textContent) {
-    const testCase = TestCaseFactory.create(Pagination, props);
-    const els = testCase.find(selector);
-
-    const el = els.find(
-      element => element.textContent === textContent
-    );
-
-    return el;
-  }
-
-  function clickOnElementWithTextContentAndExpectOnChangePage(props, selector, textContent) {
-    const testCase = TestCaseFactory.create(Pagination, props);
-    const els = testCase.find(selector);
-
-    const el = els.find(
-      element => element.textContent === textContent
-    );
-
-    testCase.trigger('click', el);
-
-    return expect(props.onChangePage)
-  }
-
   describe('First page', () => {
     it('should be disabled when this is already a first page', () => {
-      const el = elementWithTextContentForProps({
+      const { testCase, element } = render({
         currentPage: 0,
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', '<');
+      }, '<');
 
-      expect(el.className).toBe('pagination__disabled');
+      testCase.trigger('click', element);
+
+      expect(element.className).toBe('pagination__disabled');
     });
 
     it('should not be clickable when disabled', () => {
-      clickOnElementWithTextContentAndExpectOnChangePage({
+      const { props, testCase, element } = render({
         currentPage: 0,
         onChangePage: jasmine.createSpy('onChangePage'),
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', '<').not.toHaveBeenCalled();
+      }, '<');
+
+      testCase.trigger('click', element);
+
+      expect(props.onChangePage).not.toHaveBeenCalled();
     });
 
     it('should be clickable when enabled', () => {
-      clickOnElementWithTextContentAndExpectOnChangePage({
+      const { props, testCase, element } = render({
         currentPage: 5,
         onChangePage: jasmine.createSpy('onChangePage'),
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', '<').toHaveBeenCalled();
+      }, '<');
+
+      testCase.trigger('click', element);
+
+      expect(props.onChangePage).toHaveBeenCalled();
     });
   });
 
   describe('Previous page', () => {
     it('should be disabled when there is no previous page to select', () => {
-      const el = elementWithTextContentForProps({
+      const { element } = render({
         currentPage: 0,
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', 'Prev');
+      }, 'Prev');
 
-      expect(el.className).toBe('pagination__disabled');
+      expect(element.className).toBe('pagination__disabled');
     });
 
     it('should not be clickable when disabled', () => {
-      clickOnElementWithTextContentAndExpectOnChangePage({
+      const { props, testCase, element } = render({
         currentPage: 0,
         onChangePage: jasmine.createSpy('onChangePage'),
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', 'Prev').not.toHaveBeenCalled();
+      }, 'Prev');
+
+      testCase.trigger('click', element);
+
+      expect(props.onChangePage).not.toHaveBeenCalled();
     });
 
     it('should be clickable when enabled', () => {
-      clickOnElementWithTextContentAndExpectOnChangePage({
+      const { props, testCase, element } = render({
         currentPage: 5,
         onChangePage: jasmine.createSpy('onChangePage'),
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', 'Prev').toHaveBeenCalled();
+      }, 'Prev');
+
+      testCase.trigger('click', element);
+
+      expect(props.onChangePage).toHaveBeenCalled();
     });
   });
 
   describe('Current selected page', () => {
     it('should be decorated with proper class names', () => {
-      const el = elementWithTextContentForProps({
+      const { element } = render({
         currentPage: 2,
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', '3');
+      }, '3');
 
-      expect(el.className).toBe('pagination__page pagination__disabled');
+      expect(element.className).toBe('pagination__page pagination__disabled');
     });
 
     it('should not be clickable', () => {
-      clickOnElementWithTextContentAndExpectOnChangePage({
+      const { props, testCase, element } = render({
         currentPage: 2,
         onChangePage: jasmine.createSpy('onChangePage'),
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', '3').not.toHaveBeenCalled();
+      }, '3');
+
+      testCase.trigger('click', element);
+
+      expect(props.onChangePage).not.toHaveBeenCalled();
     });
   });
 
   describe('Next page', () => {
     it('should be disabled when there is no next page to select', () => {
-      const el = elementWithTextContentForProps({
+      const { element } = render({
         currentPage: 14,
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', 'Next');
+      }, 'Next');
 
-      expect(el.className).toBe('pagination__disabled');
+      expect(element.className).toBe('pagination__disabled');
     });
 
     it('should not be clickable when disabled', () => {
-      clickOnElementWithTextContentAndExpectOnChangePage({
+      const { props, testCase, element } = render({
         currentPage: 14,
         onChangePage: jasmine.createSpy('onChangePage'),
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', 'Next').not.toHaveBeenCalled();
+      }, 'Next');
+
+      testCase.trigger('click', element);
+
+      expect(props.onChangePage).not.toHaveBeenCalled();
     });
 
     it('should be clickable when enabled', () => {
-      clickOnElementWithTextContentAndExpectOnChangePage({
+      const { props, testCase, element } = render({
         currentPage: 10,
         onChangePage: jasmine.createSpy('onChangePage'),
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', 'Next').toHaveBeenCalled();
+      }, 'Next');
+
+      testCase.trigger('click', element);
+
+      expect(props.onChangePage).toHaveBeenCalled();
     });
   });
 
   describe('Last page', () => {
     it('should be disabled when this is already a last page', () => {
-      const el = elementWithTextContentForProps({
+      const { element } = render({
         currentPage: 14,
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', '>');
+      }, '>');
 
-      expect(el.className).toBe('pagination__disabled');
+      expect(element.className).toBe('pagination__disabled');
     });
 
     it('should not be clickable when disabled', () => {
-      clickOnElementWithTextContentAndExpectOnChangePage({
+      const { props, testCase, element } = render({
         currentPage: 14,
         onChangePage: jasmine.createSpy('onChangePage'),
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', '>').not.toHaveBeenCalled();
+      }, '>');
+
+      testCase.trigger('click', element);
+
+      expect(props.onChangePage).not.toHaveBeenCalled();
     });
 
     it('should be clickable when enabled', () => {
-      clickOnElementWithTextContentAndExpectOnChangePage({
+      const { props, testCase, element } = render({
         currentPage: 10,
         onChangePage: jasmine.createSpy('onChangePage'),
         totalPages: 15,
         visiblePages: 3,
-      }, 'li', '>').toHaveBeenCalled();
+      }, '>');
+
+      testCase.trigger('click', element);
+
+      expect(props.onChangePage).toHaveBeenCalled();
     });
   });
 });
